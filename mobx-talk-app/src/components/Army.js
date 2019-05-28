6ed8f104b1ed3a3f4ee2659a423fc0e0e16ab6f7
Wagner Tamas <tamas.wagner@gmail.com>
2019-05-29 00:13:21 +0300

    V1 decorators

--- mobx-talk-app/src/components/Army.js
+++ mobx-talk-app/src/components/Army.js
@@ -2,23 +2,39 @@ import React, { Component } from 'react';
 import './Army.css';
 import marine from '../images/marine.png';
 import hydra from '../images/hydra.png';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
-export default class Army extends Component {
+@observer
+class Army extends Component {
+
+  @observable cnt = 0;
+
+  handleAdd = () => {
+    this.cnt++;
+  }
+
+  handleRemove = () => {
+    this.cnt--;
+  }
+
   render() {
     const { armyModel } = this.props;
-    const { count, image, name, unitName } = armyModel;
+    const { image, name, unitName } = armyModel;
 
     return <div className='armyCell'>
       <div>
-        <h4>{`The great ${name} army of ${count} ${unitName}`}</h4>
+        <h4>{`The great ${name} army of ${this.cnt} ${unitName}`}</h4>
         <div>
-            {Array(count).fill(1).map(i => <img src={image === 'hydra' ? hydra : marine} alt='...'/>)} 
+            {Array(this.cnt).fill(1).map(i => <img src={image === 'hydra' ? hydra : marine} alt='...'/>)} 
         </div>
       </div>
       <div className='buttons'>
-        <button>Add</button>
-        <button>Remove</button>
+        <button onClick={this.handleAdd}>Add</button>
+        <button onClick={this.handleRemove}>Remove</button>
       </div>
     </div>;
   }
 }
+
+export default Army;
